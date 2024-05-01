4ef9e9e9ccfc914ef879531dc46098a8da767314
Surafeljava <surafelk27@gmail.com>
2024-05-01 13:58:33 +0300

    bet form validation

--- src/components/BetForm.js
+++ src/components/BetForm.js
@@ -18,15 +18,49 @@ function BetForm() {
         dispatch(handleBetChange({...betForm, [e.target.name]:e.target.value}))   
     }
 
+    const formValidation = (name, value) => {
+        switch (name) {
+            case 'targetMult':
+                if(isNaN(value)){
+                    return "Invalid target multiplier.";
+                }
+
+                if(value<1 || value>100){
+                    return "Must be from 1.01 to 100.00";
+                }
+                return null;
+            case 'betAmount':
+                if(isNaN(value)){
+                    return "Invalid bet amount.";
+                }
+                return null;
+            default:
+                return null;
+        }
+    }
+
+    const canBet = () => {
+        const targetCheck = formValidation('targetMult', current_bet.targetMult);
+        const betAmount = formValidation('betAmount', current_bet.betAmount);
+
+        if(!targetCheck && !betAmount){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     return (
-        <div className='w-full h-full flex flex-col p-4 gap-6'>
+        <div className='w-full h-full flex flex-col p-4 gap-4 lg:gap-6'>
             <div className='flex flex-row gap-4'>
-                <FormTextField name='betAmount' label='Bet Amount' onChangeHandler={handleInputChange} value={current_bet.betAmount}/>
-                <FormTextField name='profitToWin' label='Profit to Win' onChangeHandler={null} value={current_bet.betAmount*current_bet.targetMult}/>
+                <FormTextField name='betAmount' label='Bet Amount' formValidation={formValidation} onChangeHandler={handleInputChange} value={current_bet.betAmount} placeholder='enter amount'/>
+                <FormTextField name='profitToWin' label='Profit to Win' formValidation={formValidation} onChangeHandler={null} value={current_bet.betAmount*current_bet.targetMult}/>
             </div>
-            <FormTextField name='targetMult' label='Target Multiplier' onChangeHandler={handleInputChange} value={current_bet.targetMult}/>
-            <FormTextField name='winChance' label='Win Chance' onChangeHandler={null} value={current_bet.betAmount}/>
-            <button className='px-8 py-2 bg-primary-blue text-white rounded-lg' onClick={handleBetClick}>
+            <FormTextField name='targetMult' label='Target Multiplier' formValidation={formValidation} onChangeHandler={handleInputChange} value={current_bet.targetMult}/>
+            <FormTextField name='winChance' label='Win Chance' formValidation={formValidation} onChangeHandler={null} value={`${((100-current_bet.targetMult)/100)*100}%`} 
+            placeholder='win chance'/>
+            <button disabled={!canBet()} 
+            className='px-8 py-2 bg-primary-blue hover:opacity-80 duration-300 text-white rounded-lg' onClick={handleBetClick}>
                 Bet
             </button>
         </div>
--- src/components/Shared/FormTextField.js
+++ src/components/Shared/FormTextField.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function FormTextField({name, label, onChangeHandler, value, placeholder='0.0000000'}) {
+function FormTextField({name, label, onChangeHandler, value, placeholder='0.0000000', formValidation}) {
+
+    const [valid, setValid] = useState(true);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    useEffect(() => {
+        setValid((prev) => {
+            const res = formValidation(name, value);
+            if(res){
+                setErrorMessage(res);
+                return false;
+            }else{
+                return true;
+            }
+        });
+    // eslint-disable-next-line
+    }, [value])
+
     return (
-        <div className='flex flex-col gap-2 w-full'>
+        <div className='flex flex-col w-full'>
             <p className='text-white'>{label}</p>
-            <div className='flex-grow'>
-                <input disabled={!onChangeHandler} type='text' className='w-full py-3 px-3 rounded-lg bg-accent-blue text-slate-400 text-sm'
+            <div className='flex-grow mt-2'>
+                <input disabled={!onChangeHandler} type='text' 
+                className={`w-full py-3 px-3 rounded-lg bg-accent-blue text-slate-400 text-sm border-[1px] disabled:border-slate-400 focus:outline-none ${valid ? 'border-transparent focus:border-primary-blue' : 'border-primary-red focus:border-primary-red'}`}
                 onChange={onChangeHandler} value={value} name={name} placeholder={placeholder}/>  
             </div>
+            {!valid && <p className='text-primary-red text-[13px]'>{errorMessage}</p>}
         </div>
     );
 }
